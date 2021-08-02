067624098e6f950833a090b4420bed1cedebcee4
claireconglizhang <congliz@uoregon.edu>
2021-08-02 15:11:52 -0700

    assignment 1 key (docx and R files)

---
+++ assignments/keys/assignment01_key.R
@@ -0,0 +1,42 @@
+# Assignment 1 key
+
+# 1. Read in the dataset
+
+if (!require(pacman)) install.packages('pacman', repos = 'https://cran.rstudio.com')
+pacman::p_load(tidyverse, knitr, kableExtra)
+
+pd <- read_csv(here::here("data", "cat.csv"))
+
+# 2. Understand the structure of the data
+
+head(pd)
+# head(pd, 10) to show the first 10 observations
+# head(pd %>% arrange(desc(absenteeism)), 10) to show 10 observations with highest value on absenteeism
+
+str(pd)
+
+pd$treat <- factor(pd$treat,
+                   levels = c(0, 1),
+                   labels = c("Control", "Treat"))
+
+pd$absenteeism <- factor(pd$absenteeism,
+                         levels = c(0, 1),
+                         labels = c("No", "Yes"))
+
+pd$cgender <- factor(pd$cgender,
+                     levels = c(0, 1),
+                     labels = c("Boy", "Girl"))
+
+# 3. Describe and summarize the two key variables
+
+table(pd$treat)
+
+barplot(table(pd$treat),
+        xlab = "Experimental Group",
+        ylab = "Number of Children")
+
+prop.table(table(pd$absenteeism))
+
+barplot(prop.table(table(pd$absenteeism)),
+        xlab = "Chronically Absent",
+        ylab = "Proportion of Children")
