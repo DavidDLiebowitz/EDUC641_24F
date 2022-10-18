e6c04a15211494cfbcad1b024d1a2fc5ed455e63
David Liebowitz <daviddl@uoregon.edu>
2022-10-18 12:40:25 -0700

    Add class 7/8 slides

--- slides/EDUC641_7_code.R
+++ slides/EDUC641_7_code.R
@@ -1,9 +1,8 @@
 library(here)
 library(tidyverse)
-library(ggplot2)
 
 
-###This tells R where the script is located in relationship to the "root" directory of your project
+### This tells R where the script is located in relationship to the "root" directory of your project
 # Using this command you can then use shortened versions of file pathways that will work across different users' systems
 # A non-preferred alternative is to read the data in using the full filepath
 
@@ -17,80 +16,52 @@ i_am("slides/EDUC641_7_code.r")
 
 # Let's first access the data
 
-who <- read.csv(here("data/life_expectancy.csv")) %>% 
+who <- read.csv(here("data/life_expectancy.csv")) %>%
+  # going to do some data cleaning; first making variable names take a common format
   janitor::clean_names() %>% 
-  filter(year == 2015) %>% 
-  select(country, status, life_expectancy) %>% 
+  # filtering to focus only on 2015
+  filter(year == 2015) %>%
+  # selecting only the variables we need
+  select(country, status, life_expectancy, schooling) %>% 
+  # renaming one of the variables
   rename(region = country) %>% 
+  # rounding life expectancy to nearest year
   mutate(life_expectancy = round(life_expectancy, digits = 0))
 
+############################################################
+#####         Distributions visualized              ########
+############################################################
 
-
+##############
 ## Histogram
-ggplot(who, aes(life_expectancy)) +
+
+# Default histogram
+hist(who$life_expectancy)
+
+# Add more breaks
+hist(who$life_expectancy, breaks = 16)
+
+# Use ggplot
+ggplot(who, aes(life_expectancy)) + 
   geom_histogram()
 
-# Stem-and-leaf
+#################
+## Stem-and-leaf
 stem(who$life_expectancy)
 
 
-# Density plot
+##################
+## Density plot
 ggplot(who, aes(life_expectancy)) +
   geom_density()
 
 
-# IQR
-ggplot(who, aes(x = life_expectancy)) +
-  geom_density() +
-  geom_vline(xintercept = quantile(who$life_expectancy, .25)) +
-  geom_label(
-    x = quantile(who$life_expectancy, .25),
-    y = .03,
-    label = paste("25th %ile =", quantile(who$life_expectancy, .25))
-  ) +
-  geom_vline(xintercept = quantile(who$life_expectancy, .75)) +
-  geom_label(
-    x = quantile(who$life_expectancy, .75),
-    y = .03,
-    label = paste("75th %ile =", quantile(who$life_expectancy, .75))
-  ) +
-  geom_segment(
-    x = quantile(who$life_expectancy, .25),
-    xend = quantile(who$life_expectancy, .75),
-    y = .015,
-    yend = .015,
-    linetype = 2
-  ) +
-  geom_label(x = 71.5,
-             y = 0.01,
-             label = "IQR = 11") +
-  theme_classic()
-
-
-# Transformation
-who$life_expectancy_zscore <- 
-  (who$life_expectancy - mean(who$life_expectancy)) /
-  sd(who$life_expectancy)
-
-head(who$life_expectancy_zscore)
-hist(who$life_expectancy_zscore)
-
-#### Compare transformed and raw data
-# Raw
-ggplot(who, aes(life_expectancy)) +
-  geom_histogram()
-
-# Standardized
-who_stand <- ggplot(who, aes(life_expectancy_zscore)) +
-  geom_histogram()
-
-
-
-# Boxplot
+#################
+##  Boxplot
 boxplot(who$life_expectancy, horizontal=T)
 
 
-# Boxplot over categories
+# Boxplot over categories (in this case, years)
 ## bring in a few more years
 
 who_more <- read.csv(here("data/life_expectancy.csv")) %>% 
@@ -103,4 +74,40 @@ who_more <- read.csv(here("data/life_expectancy.csv")) %>%
 who_more %>% 
   ggplot(aes(factor(year), life_expectancy, fill = factor(year))) +
   geom_boxplot() +
-  theme_minimal()
+  theme_minimal() +
+  theme(legend.position = "none")
+
+
+
+
+
+############################################################
+#####         Measures of central tendency          ########
+
+mean(who$life_expectancy, na.rm = T)
+
+## If there are missing values of life_expectancy (NA), we will get an error.
+## So, we generally want to default to setting na.rm = T which tells R to ignore missing values
+
+## Are there any NA?
+sum(is.na(who$life_expectancy))
+
+median(who$life_expectancy)
+
+
+
+
+############################################################
+#####         Measures of variation                 ########
+
+range(who$life_expectancy)
+
+IQR(who$life_expectancy)
+
+quantile(who$life_expectancy)
+
+# By default will give quartiles
+# Can change with probs sub-command
+# For example, by quintiles
+quantile(who$life_expectancy, probs = seq(0, 1, 0.2))
+
